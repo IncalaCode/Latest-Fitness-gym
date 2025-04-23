0969067578f1a392bbf9e371fdffa0db04df6d08
= <=>
2025-04-23 06:23:30 +0300

    ready to deply

--- back-end/server.js
+++ back-end/server.js
@@ -23,6 +23,11 @@ app.use('/uploads', (req, res, next) => {
 }, express.static(path.join(__dirname, 'uploads')));
 
 
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 
 app.use('/api', routes);
 
--- front-end/src/config/config.js
+++ front-end/src/config/config.js
@@ -1,6 +1,6 @@
 
-export const API_URL = 'http://localhost:3001/api';
-export const IMAGE_URL = 'http://localhost:3001';
+export const API_URL = 'http://latestfitnessethiopia.com/api';
+export const IMAGE_URL = 'http://latestfitnessethiopia.com';
 export const AUTH_ENDPOINTS = {
   LOGIN: `${API_URL}/auth/login`,
   REGISTER: `${API_URL}/users/register`,
