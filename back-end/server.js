bdce9a0665e0c603ada9e5b4d5effcece4f05314
= <=>
2025-04-23 09:34:20 +0300

    ready to deply latest4

--- back-end/server.js
+++ back-end/server.js
@@ -23,9 +23,9 @@ app.use('/uploads', (req, res, next) => {
 }, express.static(path.join(__dirname, 'uploads')));
 
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname)));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 
