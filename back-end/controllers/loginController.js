bd86c000c4a461f43ba695904584589e1de54a06
= <=>
2025-04-23 11:17:08 +0300

    fixing the login

--- back-end/controllers/loginController.js
+++ back-end/controllers/loginController.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const db = require('../models');
 const { Admin, User } = db;
+require('dotenv').config();
 
 exports.login = async (req, res, next) => {
   try {
